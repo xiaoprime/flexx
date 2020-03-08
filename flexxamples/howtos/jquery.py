0475ffa126cb11910bf1127085c0196a10c2ef8a
Prime <31033455+xiaoprime@users.noreply.github.com>
2020-03-08 21:13:07 +0800

    add AutoComplete

--- flexxamples/howtos/jquery.py
+++ flexxamples/howtos/jquery.py
@@ -7,7 +7,7 @@ This demonstrates how Flexx can interact wih other JS frameworks.
 from pscript import RawJS
 
 from flexx import flx
-
+from flexx import event
 
 # Associate assets needed by this app.
 flx.assets.associate_asset(__name__, "http://code.jquery.com/jquery-1.10.2.js")
@@ -25,6 +25,32 @@ class DatePicker(flx.Widget):
         return node
 
 
+class AutoComplete(flx.Widget):
+
+    options = event.ListProp(['yes', 'no', 'I have no idea.'], settable=True, doc="""
+        """)
+
+    def _create_dom(self):
+        global window
+        self.node = window.document.createElement('input')
+        RawJS('$')(self.node).autocomplete({'source': self.options})
+        self._addEventListener(self.node, 'blur', self.user_done, False)
+        return self.node
+
+    @event.reaction('options')
+    def refresh(self):
+        RawJS('$')(self.node).autocomplete({'source': self.options})
+
+
+    @event.emitter
+    def user_done(self):
+        """ Event emitted when the user is done editing the text, either by
+        moving the focus elsewhere, or by hitting enter.
+        Has ``old_value`` and ``new_value`` attributes (which are the same).
+        """
+        d = {'key': self.node.value}
+        return d
+
 class Example(flx.Widget):
 
     def init(self):
@@ -32,6 +58,7 @@ class Example(flx.Widget):
         with flx.FormLayout():
             self.start = DatePicker(title='Start date')
             self.end = DatePicker(title='End date')
+            self.auto = AutoComplete(title='')
             flx.Widget(flex=1)
 
 
